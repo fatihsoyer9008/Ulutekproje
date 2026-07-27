609ae60380f0d37002af64de678bb11b02c5c86a
Zafer Tuna Mercanoğlu <032290064@ogr.uludag.edu.tr>
2026-07-27 12:30:30 +0300

    Export ve test eklendi

--- finance_database/test/finance_database_test.dart
+++ finance_database/test/finance_database_test.dart
@@ -1,12 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:finance_database/finance_database.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('TransactionEntity alanları doğru atanıyor', () {
+    final entity = TransactionEntity()
+      ..amountInMinor = 1250
+      ..category = TransactionCategory.market
+      ..date = DateTime(2026, 7, 27)
+      ..merchantName = 'Migros'
+      ..source = TransactionSource.ocrRegex
+      ..createdAt = DateTime.now()
+      ..updatedAt = DateTime.now();
+
+    expect(entity.amountInMinor, 1250);
+    expect(entity.category, TransactionCategory.market);
+    expect(entity.merchantName, 'Migros');
+    expect(entity.source, TransactionSource.ocrRegex);
   });
 }
